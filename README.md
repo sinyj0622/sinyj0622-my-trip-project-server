a8a1ac96ff53a2e91abe51668385ab55cde3e78d
eunjeong <euuun22@naver.com>
2020-02-07 19:10:02 +0900

    자바 서버 프로젝트 생성

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sinyj0622-my-trip-project-server
+
+- 서버 여행 관리 시스템입니다
---
+++ src/main/java/sinyj0622/mytrip/ServerApp.java
@@ -0,0 +1,9 @@
+package sinyj0622.mytrip;
+
+public class ServerApp {
+
+
+  public static void main(String[] args) {
+    System.out.println("서버 여행 관리 시스템입니다");
+  }
+}
